ee50342e3b0f42b91002e7465ff4f207437aaee0
Jeroen_B <jeroen.200@hotmail.com>
2017-04-25 17:23:05 +0200

    Initial Angular Material setup

--- Oculus/src/app/app.module.ts
+++ Oculus/src/app/app.module.ts
@@ -2,19 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { HomeComponent } from './home/home.component';
+
+import { UserComponent } from './Energie-Monitor/user/user.component';
+import { DashboardComponent } from './Energie-Monitor/dashboard/dashboard.component';
+import { LoginComponent } from './Energie-Monitor/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    DashboardComponent,
+    UserComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MaterialModule.forRoot(),
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
