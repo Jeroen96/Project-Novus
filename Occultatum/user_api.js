d6b0de9dc2e6bca184278b73e0889f576cc3ee4b
Jeroen_B <jeroen.200@hotmail.com>
2017-07-21 18:01:24 +0200

    Added login route Fixed mysql connection not closing upon empty request body

--- Occultatum/user_api.js
+++ Occultatum/user_api.js
@@ -2,9 +2,10 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jwt-simple');
 var mysql = require('mysql');
+var bcrypt = require('bcrypt');
 
 // General mysql connect funtion
-function connectDb(req) {
+function connectToDb(req) {
     var config = req.app.get('config');
     var connection = mysql.createConnection(config.dbInfo);
     connection.connect(function (err) {
@@ -18,33 +19,87 @@ function connectDb(req) {
 
 // Body: {username: '',password: ''}
 router.post('/createAccount', function (req, res) {
-    var connection = connectDb(req);
     var username = req.body.username;
     var password = req.body.password;
     // Check if username and password are both available
     if (username === '' || username === undefined || password === '' || password === undefined) {
         res.status(400);
-        res.json({ error: 'username and or password is empty or missing!' });
+        res.json({ error: 'username and or password is/are empty or missing!' });
+        return;
     }
+    // Hash password to be saved in database
+    var connection = connectToDb(req);
+    bcrypt.hash(password, req.app.get('config').saltRounds, function (err, passwordHash) {
+        // Insert name and hashed password into pending table when username is available
+        console.log(passwordHash);
+        var query = 'INSERT INTO pending (username, password) SELECT * FROM (SELECT ?,?) as tmp '
+            + 'WHERE NOT EXISTS (SELECT username FROM pending WHERE username = ? UNION SELECT username from users WHERE username = ?)';
+        connection.query(query, [username, passwordHash, username, username], function (err, results) {
+            if (err) {
+                console.log(err);
+                res.status(500);
+                res.json('An error occured');
+                connection.end();
+                return;
+            }
+            if (results.affectedRows > 0) {
+                res.status(201);
+                res.json('Account created');
+            } else {
+                res.status(409);
+                res.json('Account name already exists');
+            }
+        });
+        connection.end();
+    });
+});
 
-    // Insert name into db when username is available
-    var query = 'INSERT INTO pending (username, password) SELECT * FROM (SELECT ?,?) as tmp '
-        + 'WHERE NOT EXISTS (SELECT username FROM pending WHERE username = ? UNION SELECT username from users WHERE username = ?)';
-    connection.query(query, [username, password, username, username], function (err, results) {
+// Body {username: '', password: ''}
+router.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    // Check if username and password are both available
+    if (username === '' || username === undefined || password === '' || password === undefined) {
+        res.status(400);
+        res.json({ error: 'username and or password is/are empty or missing!' });
+        return;
+    }
+    var connection = connectToDb(req);
+    var query = 'SELECT * FROM users WHERE username = ?';
+    connection.query(query, [username], function (err, results) {
         if (err) {
-            console.log(err);
+            res.status(500);
+            res.json('An error occured');
+            connection.end();
             return;
         }
-        if (results.affectedRows > 0) {
-            res.status(201);
-            res.json('Account created');
+        // If given username exists in db check password hash with given password
+        if (results.length > 0) {
+            var hashResult = results[0].password;
+            var userRights = results[0].userRights;
+            bcrypt.compare(password, hashResult, function (err, result) {
+                // Password hash matches password, return jwt token
+                if (result) {
+                    var token = jwt.encode({
+                        'iss': username,
+                        'usr': userRights
+                    }, req.app.get('config').jwtKey);
+                    res.status(200);
+                    res.json({ 'token': token });
+                } else {
+                    res.status(401);
+                    res.json('Authorisation failed');
+                }
+            });
         } else {
-            res.status(409);
-            res.json('Account name already exists');
+            res.status(404);
+            res.json('User not found');
         }
     });
     connection.end();
 });
+
+// TODO: Add token checking middleware after this line
 
 // // add user to pending table
 // var query = 'SELECT * FROM users WHERE username = ?';
